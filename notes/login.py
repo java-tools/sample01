b9b7b87bf26bf8f6b9463e75d64b2e8ab7ec6970
Bala Raman <srbala@gmail.com>
2022-10-18 15:02:10 -0400

    script upd
    Signed-off-by: Bala Raman <srbala@gmail.com>

--- notes/login.py
+++ notes/login.py
@@ -10,12 +10,14 @@ def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_in
 
     # create a locator
     get_started = page.locator("text=login")
-
+    print("got page ...")
     # Expect an attribute "to be strictly equal" to the value.
     expect(get_started).to_have_attribute("href", "/login")
 
     # Click the get started link.
     get_started.click()
 
+    print("click on link login")
     # Expects the URL to contain intro.
     expect(page).to_have_url(re.compile(".*login"))
+    print("click comlete ...")
